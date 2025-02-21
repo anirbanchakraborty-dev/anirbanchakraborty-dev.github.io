716dc6c28d0286819de42f0736bc150f61924809
Anirban Chakraborty <achakraborty2591@gmail.com>
2025-02-21 12:22:21 -0800

    refactor: update class names for consistency and improve hamburger menu functionality; add .gitignore for .DS_Store files

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-function toggle_hamburger_menu_button_icon() {
-  const menu = document.querySelector(
-    ".webpage_hamburger_menu_button_icon_links"
-  );
-  const icon = document.querySelector(".webpage_hamburger_menu_button_icon");
+function toggleMenu() {
+  const menu = document.querySelector(".hamburger_menu_links");
+  const icon = document.querySelector(".hamburger_icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
 
 document.addEventListener("click", function (event) {
-  const menu = document.querySelector(
-    ".webpage_hamburger_menu_button_icon_links"
-  );
-  const icon = document.querySelector(".webpage_hamburger_menu_button_icon");
+  const menu = document.querySelector(".hamburger_menu_links");
+  const icon = document.querySelector(".hamburger_icon");
   const isClickInsideMenu = menu.contains(event.target);
   const isClickInsideIcon = icon.contains(event.target);
 
